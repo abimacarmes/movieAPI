4f8aff5687e654af94f83fc1ad7bdf0b420cfeca
Abigail M-A <a.mackenzie.armes@gmail.com>
2021-02-07 14:55:43 -0500

    server deployment

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ console.log(process.env.API_TOKEN)
 
 const app = express()
 
-app.use(morgan('dev'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+
+app.use(morgan(morganSetting));
 app.use(helmet());
 app.use(cors());
 
@@ -55,10 +57,20 @@ function handleGetMovie(req,res){
     res.json(response)
 };
 
+app.use((error, req, res, next)=> {
+    let response
+    if(process.env.NODE_ENV === 'production'){
+        response = {error: {message: 'server error'}}
+    }
+    else{
+        response = {error}
+    }
+    res.status(500).json(response)
+})
 
 app.get('/movies', handleGetMovie);
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`)
